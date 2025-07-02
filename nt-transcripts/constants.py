32301c6b1396a0d1b7cf3aab38777350cc6d9d87
Christoph Werner <christoph.werner@hs-wismar.de>
2025-07-02 10:15:46 +0200

    Add nt-transcripts for downloading transcription files

---
+++ nt-transcripts/constants.py
@@ -0,0 +1,24 @@
+IGNTP_PATH = "./igntp"
+IGNTP_1COR = IGNTP_PATH + "/ecm_1corinthians"
+IGNTP_COL = IGNTP_PATH + "/ecm_colossians"
+IGNTP_EPH = IGNTP_PATH + "/ecm_ephesians"
+IGNTP_GAL = IGNTP_PATH + "/ecm_galatians"
+IGNTP_JOHN = IGNTP_PATH + "/ecm_john"
+IGNTP_ROM = IGNTP_PATH + "/ecm_romans"
+IGNTP_PHIL = IGNTP_PATH + "/ecm_philippians"
+IGNTP_DIRS = [
+    IGNTP_JOHN,
+    IGNTP_ROM,
+    IGNTP_1COR,
+    IGNTP_GAL,
+    IGNTP_EPH,
+    IGNTP_PHIL,
+    IGNTP_COL,
+]
+
+NTVMR_INDEX_URL = "https://ntvmr.uni-muenster.de/community/vmr/api/metadata/liste/get/"
+NTVMR_PATH = "./ntvmr"
+OVERWRITE = True
+ERROR_LOG = "./error.log"
+ALL = True
+MAX_WORKERS = 10
