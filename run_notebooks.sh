7a40edd95ddf0264f69f9cde8550f20fdc1db04c
Christoph Werner <christoph.werner@hs-wismar.de>
2025-07-02 10:49:42 +0200

    Update run_notebooks script

--- run_notebooks.sh
+++ run_notebooks.sh
@@ -27,35 +27,63 @@ cd scripts
 
 # Replace 'from tqdm.notebook import tqdm' with 'from tqdm import tqdm' in all Python files
 find . -type f -name "*.py" -exec sed -i "s/from tqdm\.notebook import tqdm/from tqdm import tqdm/g" {} +
+cd ..
 
 # Define functions for each task
+
+get_transcripts() {
+    echo "Running task: Get transcripts"
+    cd ./nt-transcripts/
+    python ./download.py
+    cd ..
+}
+
+get_manuscripts() {
+    echo "Running task: Get manuscripts"
+    cd ./nt-manuscripts/
+    python ./download.py
+    cd ..
+}
+
 get_words() {
     echo "Running task: Get words"
+    cd scripts
     python ./get_words.py
+    cd ..
 }
 
 parse_tei() {
     echo "Running task: Parse TEI for verses"
+    cd scripts
     python ./parse_tei.py
+    cd ..
 }
 parse_json() {
     echo "Running task: Parse JSON for manuscript meta data"
+    cd scripts
     python ./parse_json.py
+    cd ..
 }
 parse_sparql() {
     echo "Running task: Parse sparql for manuscript meta data"
+    cd scripts
     python ./enhance_sparql.py
+    cd ..
 }
 
 search_occurrences() {
     echo "Running task: Search occurrences"
+    cd scripts
     python ./ecm-na28-processing.py
     python ./search_occurrences.py
+    cd ..
 }
 
 publication_prep() {
     echo "Running task: Publication preparation"
+    cd scripts
     python ./pub_prep.py
+    cd ..
 }
 
 # Check if --noninteractive flag is provided
@@ -70,12 +98,14 @@ if [[ "$1" == "--noninteractive" ]]; then
 else
     # Prompt user for input
     read -p "Enter the tasks to run separated by commas: 
-1: Get words
-2: Parse TEI for manuscript and verse data
-3: Parse JSON for manuscript meta data
-4: Parse sparql for manuscript meta data
-5: Search occurrences
-6: Publication preparation 
+1: Get transcripts
+2: Get manuscript data
+3: Get words
+4: Parse TEI for manuscript and verse data
+5: Parse JSON for manuscript meta data
+6: Parse sparql for manuscript meta data
+7: Search occurrences
+8: Publication preparation 
 Input: " user_input
 
     # Split input into an array
@@ -84,12 +114,14 @@ Input: " user_input
     # Execute tasks based on user input
     for task in "${tasks[@]}"; do
         case $task in
-            1) get_words ;;
-            2) parse_tei ;;
-            3) parse_json ;;
-            4) parse_sparql ;;
-            5) search_occurrences ;;
-            6) publication_prep ;;
+            1) get_transcripts ;;
+            2) get_manuscripts ;;
+            3) get_words ;;
+            4) parse_tei ;;
+            5) parse_json ;;
+            6) parse_sparql ;;
+            7) search_occurrences ;;
+            8) publication_prep ;;
             *) echo "Invalid task: $task" ;;
         esac
     done
